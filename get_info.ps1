9fd678efa6acbb7d9dde6137cdf36358ac7ed55e
FerdiGul <gulferdi.software@gmail.com>
2019-01-09 00:25:13 +0300

    Update get_info.ps1

--- get_info.ps1
+++ get_info.ps1
@@ -1,3 +1,4 @@
 Get-WmiObject -Class win32_LocalTime
 Get-Process
 Get-WmiObject -Class win32_UserAccount
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName . | Format-Table -Property IPAddress
