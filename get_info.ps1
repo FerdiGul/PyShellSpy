6b42787ab3393870017cc811cc206358cb7483fb
FerdiGul <gulferdi.software@gmail.com>
2019-01-10 00:09:20 +0300

    Update get_info.ps1

--- get_info.ps1
+++ get_info.ps1
@@ -1,7 +1,6 @@
 Get-WmiObject -Class win32_LocalTime
-$rt=$rt -join "`n"
-$rt=$rt -join "`n"
-$rt=$rt -join "`n"
+$str +="`n"
+$str
 Get-Process
 Get-WmiObject -Class win32_UserAccount
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName . | Format-Table -Property IPAddress
