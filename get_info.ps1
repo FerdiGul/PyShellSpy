a1fc7401175414e9aaa77e1ff52ef8cfbdfba61a
FerdiGul <gulferdi.software@gmail.com>
2019-01-09 23:54:48 +0300

    Update get_info.ps1

--- get_info.ps1
+++ get_info.ps1
@@ -3,4 +3,4 @@ Get-Process
 Get-WmiObject -Class win32_UserAccount
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName . | Format-Table -Property IPAddress
 Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion
-shutdown.exe -l
+Get-Service
