96798a074dffb7c5e973556cd028176958d85094
FerdiGul <gulferdi.software@gmail.com>
2019-01-10 00:00:37 +0300

    Update get_info.ps1

--- get_info.ps1
+++ get_info.ps1
@@ -1,6 +1,10 @@
-Get-WmiObject -Class win32_LocalTime,
-Get-Process,
-Get-WmiObject -Class win32_UserAccount,
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName . | Format-Table -Property IPAddress,
-Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion,
-Get-Service,
+Get-WmiObject -Class win32_LocalTime
+$rt=$rt -join "`n"
+$rt=$rt -join "`n"
+$rt=$rt -join "`n"
+Get-Process
+Get-WmiObject -Class win32_UserAccount
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=$true -ComputerName . | Format-Table -Property IPAddress
+Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion
+Get-Service
+
