9a9eda968363920112b6df7073857a49c22706e2
FerdiGul <gulferdi.software@gmail.com>
2019-01-13 10:03:05 +0300

    Update PyShellSpy.py

--- PyShellSpy.py
+++ PyShellSpy.py
@@ -1,5 +1,5 @@
 import argparse,smtplib 
-import subprocess as sunproc
+import subprocess as subproc
 import urllib.request
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText 
